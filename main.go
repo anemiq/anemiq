155ce95d2be6c45d53f743a888b9bca3a7a72d70
hvalls <hvalls198@gmail.com>
2018-01-22 21:27:22 +0100

    remove log

--- main.go
+++ main.go
@@ -10,38 +10,32 @@ import (
 	"github.com/anemiq/server"
 )
 
-func log(msg string) {
-	fmt.Printf(msg + "\n")
-}
-
 func main() {
 
-	log("reading config from anemiq.yml...")
+	fmt.Printf("reading config from anemiq.yml..." + "\n")
 	conf, err := config.Read()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "error: %v\n", err)
 		os.Exit(1)
 	}
-	log("done!")
+	fmt.Printf("done" + "\n")
 
-	log("connecting to database...")
+	fmt.Printf("connecting to database..." + "\n")
 	db, err := database.Open(conf.Database)
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
-	log("done!")
+	fmt.Printf("done" + "\n")
 
-	//Collect tables
 	tables, err := db.Tables(conf.Tables)
 	if err != nil {
 		panic(err)
 	}
 
-	//Generate GraphQL schema
 	sch := schema.ForTables(tables)
 
 	sv := server.New(conf, sch)
-	log("server started at http://localhost:" + conf.Server.Port + "/graphql")
+	fmt.Printf("server started at http://localhost:" + conf.Server.Port + "/graphql" + "\n")
 	sv.Start()
 }
